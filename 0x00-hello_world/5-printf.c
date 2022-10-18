3ac4b5e67943cefc22420c2e23f2a8fa82fc4830
Vultri84 <vultri84@gmail.com>
2022-10-17 20:53:07 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
-#include <stdio.h>                                                                                                                                                                                        
-/**                                                                                                                                                                                                       
- * main - Entry                                                                                                                                                                              
- * Return: Always 0                               
- */                                 
-int main(void)                                                                                                 
-{                                                                                                                                                                                                         
-	        printf("with proper grammar, but the outcome is a piece of art,\n");                                                                                                                              		        return (0);                                                                                                                                                                
+#include <stdio.sh>
+/**
+ * main - program entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
