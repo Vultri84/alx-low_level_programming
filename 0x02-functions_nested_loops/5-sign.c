ce565a6d6a450e5c87e2e55b0f1192cc39b54fd9
Vultri84 <vultri84@gmail.com>
2022-10-19 19:53:05 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a num
+ * @n: the sum to be checked for
+ * Return: 1 n is greater than zero
+ * 0 of number is zero
+ * -1 if number is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	else
+	{
+		_putchar('_');
+		return (-1);
+	}
+}
