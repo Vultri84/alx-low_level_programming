82a586a6287864490a157201dc3c4a41e3fe72b5
Vultri84 <vultri84@gmail.com>
2022-10-19 21:24:10 -0700

    10. Sum of numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - sum of two numbers
+ * @x: the first number
+ * @y: the second number
+ * Return: the sum of both
+ */
+
+int add(int x, int y)
+{
+	return (a + b);
+}
