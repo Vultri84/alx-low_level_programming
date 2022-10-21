2ec3de1190a5fefdac0e973fb98068c5d56f2629
Vultri84 <vultri84@gmail.com>
2022-10-21 16:40:29 -0700

    Isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters.
+ * @c: The character to be seeked.
+ *
+ * Returns: 1 if character is uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
