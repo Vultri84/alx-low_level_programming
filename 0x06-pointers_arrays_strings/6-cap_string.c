9fa2147a6f82c1fa6f6e23b9399985905892fa90
Vultri84 <vultri84@gmail.com>
2022-10-27 17:06:52 -0700

    Fixed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 
-			i++;
+				i++;
 		if (str[i - 1] == ' ' ||
 			str[i - 1] == '\t' ||
 			str[i - 1] == '\n' ||
@@ -29,7 +29,8 @@ char *cap_string(char *str)
 			str[i - 1] == '{' ||
 			str[i - 1] == '}' ||
 			i == 0)
-				str[i] -= 32
+				str[i] -= 32;
+
 		i++;
 	}
 
