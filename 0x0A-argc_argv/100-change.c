6fa7f95fe414b524ad57d7faeadd53709b52ee00
Vultri84 <vultri84@gmail.com>
2022-11-09 16:13:14 -0800

    fixed task5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,42 +11,34 @@
 
 int main(int argc, char *argv[])
 {
-/*Declaring variables*/
-int position, total, change, aux;
-int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-position = total = change = aux = 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
+	num = atoi(argv[1]);
+	result = 0;
 
-total = atoi(argv[1]); /*Covert str to int*/
 
-if (total <= 0)
-{
-printf("0\n");
-return (0);
-}
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-/*Declaring While*/
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
 
-while (coins[position] != '\0')
-
-{
-if (total >= coins[position])
-}
-aux = (total / coins[position]);
-change += aux;
-total -= coins[position] * aux;
-}
-
-position++;
-
-}
-
-printf("%d\n", change);
-return (0);
+	printf("%d\n", result);
+	return (0);
 }
